898807f4c0fab207d31c03eba34ec0ca27d2ecaf
Irina <alexxa@users.noreply.github.com>
2017-10-19 10:43:28 +0200

    update search of failed modules

--- tests/parse_output.sh
+++ tests/parse_output.sh
@@ -2,7 +2,7 @@
 
 installed_modules='installed_modules.log'
 enabled_modules='enabled_modules.log'
-failed_modules='error.log'
+failed_modules=$(awk -F"[()]" '{print $2}' error.log | cut -d'=' -f2 | sort | uniq)
 failed_modules_amount=$(cat $failed_modules | wc -l)
 
 sed '/Last metadata expiration/Q' installed_modules.log
@@ -12,7 +12,7 @@ printf "\n%-25s =>  %-25s\n" "MODULE" "STATE"
 echo "----------------------------------------------------------------------"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-        if $(grep -q "$line" "$failed_modules"); then
+        if $(grep -q "^$line$" "$failed_modules"); then
             printf "%-25s =>  %-25s\n" "$line" "Error: See logs [1, 2]"
 	elif $(grep -q "$line" "$installed_modules"); then
 	    printf "%-25s =>  %-25s\n" "$line" "installed"
