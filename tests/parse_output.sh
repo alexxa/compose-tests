3e2607303b853589616b78c4aa465230803205d8
Irina Gulina <igulina@redhat.com>
2017-10-20 13:05:55 +0200

    test install all profiles

--- tests/parse_output.sh
+++ tests/parse_output.sh
@@ -2,7 +2,7 @@
 
 installed_modules='installed_modules.log'
 enabled_modules='enabled_modules.log'
-failed_modules=$(awk -F"[()]" '{print $2}' error.log | cut -d'=' -f2 | sort | uniq)
+failed_modules=$(awk -F"[()]" '{print $2}' error.log | cut -d'=' -f2 | sed s,/default,, | sort | uniq)
 failed_modules_amount=$(cat $failed_modules | wc -l)
 
 sed '/Last metadata expiration/Q' installed_modules.log
@@ -12,13 +12,13 @@ printf "\n%-25s =>  %-25s\n" "MODULE" "STATE"
 echo "----------------------------------------------------------------------"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-        if $(grep -q "^$line$" "$failed_modules"); then
+        if echo "$failed_modules" | grep -q "^$line$"; then
             printf "%-25s =>  %-25s\n" "$line" "Error: See logs [1, 2]"
-	elif $(grep -q "$line" "$installed_modules"); then
-	    printf "%-25s =>  %-25s\n" "$line" "installed"
-	elif $(grep -q "$line" "$enabled_modules"); then
+        elif grep -q "$line" "$installed_modules"; then
+            printf "%-25s =>  %-25s\n" "$line" "installed"
+        elif grep -q "$line" "$enabled_modules"; then
             printf "%-25s =>  %-25s\n" "$line" "enabled"
-	else
+        else
             printf  "%-25s =>  %-25s\n" "$line" "Error: something is wrong"
 	fi
 done < "$1"
